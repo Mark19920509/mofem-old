1936ad23aa144442514ad41501efcf4cfe206e1a
woggy89 <mohamed.moussa89@gmail.com>
2015-03-09 10:04:31 +0100

    Testing merging

--- src/main.cpp
+++ src/main.cpp
@@ -43,6 +43,7 @@ int main() {
     Input::addNode(input, 0, 1, 0);
 
     // Element definition
+    // Test
     result = Input::addElement(input, Element::TRUSS_LINEAR, 0, { 1.0 }, { 0, 1 });
     result = Input::addElement(input, Element::TRUSS_LINEAR, 0, { 1.0 }, { 1, 2 });
     result = Input::addElement(input, Element::TRUSS_LINEAR, 0, { 1.0 }, { 2, 3 });
